55987df41ad5750166cde29f98c131397e4b23a7
fmupa-iscteiul <fmupa@iscte-iul.pt>
2019-10-07 09:59:05 +0100

    Criada a classe Fire

---
+++ Fire Simulator/src/Fire.java
@@ -0,0 +1,24 @@
+import Simulator.FireSimulatorObject;
+import Simulator.Updatable;
+import pt.iul.ista.poo.utils.Point2D;
+
+public class Fire extends FireSimulatorObject implements Updatable{
+
+	public Fire(Point2D position) {
+		super(position);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
